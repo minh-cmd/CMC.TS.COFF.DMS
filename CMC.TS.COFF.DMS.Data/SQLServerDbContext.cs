38af9fea1ad1862ceaea3c5613f20ab6ce77ab13
bui hieu minh <thanhniende@gmail.com>
2026-07-22 11:36:50 +0700

    feat(Data/SQLServerDbContext): initialize and create Documents table

---
+++ CMC.TS.COFF.DMS.Data/SQLServerDbContext.cs
@@ -0,0 +1,52 @@
+﻿using CMC.TS.COFF.DMS.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Data
+{
+    public class SQLServerDbContext : DbContext
+    {
+        DbSet<Documents> documents;
+        public SQLServerDbContext(DbContextOptions<SQLServerDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public SQLServerDbContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Documents>(entity =>
+            {
+                entity.ToTable("Documents");
+
+                entity.HasKey(d => d.Id);
+
+                // Title: Required with max length
+                entity.Property(d => d.Title)
+                      .IsRequired()
+                      .HasMaxLength(255);
+
+                // Description: Optional
+                entity.Property(d => d.Description)
+                      .HasMaxLength(1000);
+
+                entity.Property(d => d.ContentType)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(d => d.Extension)
+                      .IsRequired()
+                      .HasMaxLength(10);
+            });
+        }
+    }
+}
