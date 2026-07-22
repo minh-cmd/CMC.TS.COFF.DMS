e1ddf30a2caa9a87d6358e45fb1d67240e3ecf25
bui hieu minh <thanhniende@gmail.com>
2026-07-22 11:28:27 +0700

    unknow(): having commit and tracked the Data.Model/Documents already. It seem like a name change adding "s" to the name. Make it behave weirdly

---
+++ CMC.TS.COFF.DMS.Data.Model/Documents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Data.Model
+{
+    public class Documents
+    {
+        public Guid Id = Guid.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string ContentType { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+
+        public Documents()
+        {
+
+        }
+
+        public Documents(string title, string description, string contenttype, string extension)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            ContentType = contenttype;
+            Extension = extension;
+        }
+    }
+}
