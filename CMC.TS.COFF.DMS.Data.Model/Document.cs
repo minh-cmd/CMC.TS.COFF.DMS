9c137ed34093dd18ca3a7a84851d41321c25c21e
bui hieu minh <thanhniende@gmail.com>
2026-07-22 10:40:00 +0700

    feat(Data.Model): adding Document model

---
+++ CMC.TS.COFF.DMS.Data.Model/Document.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Data.Model
+{
+    internal class Document
+    {
+        internal Guid Id = Guid.Empty;
+        internal string Title { get; set; } = string.Empty;
+        internal string? Description { get; set; }
+        public string ContentType { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+
+        internal Document()
+        {
+
+        }
+
+        internal Document(string title, string description, string contenttype, string extension)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            ContentType = contenttype;
+            Extension = extension;
+        }
+    }
+}
