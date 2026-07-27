e92d493a3cbccb3d2ee2f3a92129a0b412fd79ae
bui hieu minh <thanhniende@gmail.com>
2026-07-27 17:04:12 +0700

    Refactor(Biz/IServices/IdocumentsService.cs): simplify NewDocument parameter type using namespace import

--- CMC.TS.COFF.DMS.Biz/IServices/IDocumentsService.cs
+++ CMC.TS.COFF.DMS.Biz/IServices/IDocumentsService.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CMC.TS.COFF.DMS.Biz.Model.Documents;
 
 namespace CMC.TS.COFF.DMS.Biz.IServices
 {
     public interface IDocumentsService
     {
-        Task<bool> NewDocument(CMC.TS.COFF.DMS.Biz.Model.Documents.New news);
+        Task<bool> NewDocument(New news);
     }
 }
