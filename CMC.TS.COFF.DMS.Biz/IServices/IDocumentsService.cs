06d1777b1844c8d674f8d8726e81625c41a8ef83
bui hieu minh <thanhniende@gmail.com>
2026-07-27 14:27:08 +0700

    feat(services): add IDocumentsService interface with insert operation

---
+++ CMC.TS.COFF.DMS.Biz/IServices/IDocumentsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Biz.IServices
+{
+    public interface IDocumentsService
+    {
+        Task<bool> NewDocument(CMC.TS.COFF.DMS.Biz.Model.Documents.New news);
+    }
+}
