11c0de555d5996e7e355bbdb7dd25854044892a4
bui hieu minh <thanhniende@gmail.com>
2026-07-27 14:24:57 +0700

    feat(Biz/Repository/DocumentsRepository): implement DocumentsRepository with insert method

---
+++ CMC.TS.COFF.DMS.Biz/Repositories/DocumentRepository.cs
@@ -0,0 +1,26 @@
+﻿using CMC.TS.COFF.DMS.Biz.IRepositories;
+using CMC.TS.COFF.DMS.Data;
+using CMC.TS.COFF.DMS.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Biz.Repositories
+{
+    public class DocumentRepository : IDocumentsRepository
+    {
+        private readonly SQLServerDbContext _context;
+
+        public DocumentRepository(SQLServerDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Create(Documents document)
+        {
+            _context.documents.Add(document);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+    }
+}
