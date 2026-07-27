46de051cf43e34423a7ba6b026a9628eaeba40e7
bui hieu minh <thanhniende@gmail.com>
2026-07-27 14:21:44 +0700

    feat(Biz/IRepository/IDocumentsRepository): initial create interface and adding insert opperation

---
+++ CMC.TS.COFF.DMS.Biz/IRepositories/IDocumentsRepository.cs
@@ -0,0 +1,12 @@
+﻿using CMC.TS.COFF.DMS.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Biz.IRepositories
+{
+    public interface IDocumentsRepository
+    {
+        Task<bool> Create(Documents documents);
+    }
+}
