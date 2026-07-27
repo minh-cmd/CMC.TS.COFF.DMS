1907ca4893705b96d244a303eff84b04e2730643
bui hieu minh <thanhniende@gmail.com>
2026-07-27 14:08:16 +0700

    fix(Biz.Model/Document/New): fixing the visibility of the DTO and add missing DataAnnotations namespace import

--- CMC.TS.COFF.DMS.Biz.Model/Documents/New.cs
+++ CMC.TS.COFF.DMS.Biz.Model/Documents/New.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CMC.TS.COFF.DMS.Biz.Model.Documents
 {
-    internal class New
+    public class New
     {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(255)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ContentType { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10)]
+        public string Extension { get; set; } = string.Empty;
     }
 }
