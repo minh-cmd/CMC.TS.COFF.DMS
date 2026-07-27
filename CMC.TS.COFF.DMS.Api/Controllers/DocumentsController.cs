4794eba9065a79bb983719debc68356dff4c93ae
bui hieu minh <thanhniende@gmail.com>
2026-07-27 14:33:01 +0700

    feat(Api/Controllers/DocumentsController): create controller and add POST endpoint

---
+++ CMC.TS.COFF.DMS.Api/Controllers/DocumentsController.cs
@@ -0,0 +1,31 @@
+﻿using CMC.TS.COFF.DMS.Biz.IServices;
+using CMC.TS.COFF.DMS.Biz.Model.Documents;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CMC.TS.COFF.DMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly IDocumentsService _documentsService;
+
+        public DocumentsController (IDocumentsService documentsService)
+        {
+            _documentsService = documentsService;
+        }
+        // GET: api/<DocumentsController>
+        [HttpPost]
+        public async Task<IActionResult> AddNewDocument(New news)
+        {
+            bool isSuccess = await _documentsService.NewDocument(news);
+            if (isSuccess)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
