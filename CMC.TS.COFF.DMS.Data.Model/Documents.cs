cc1d84219253d83a2f5c8fa5a1e7660796a2eb5f
bui hieu minh <thanhniende@gmail.com>
2026-07-29 16:42:11 +0700

    feat(Data.Model/Documents): add metadata properties to Documents entity

--- CMC.TS.COFF.DMS.Data.Model/Documents.cs
+++ CMC.TS.COFF.DMS.Data.Model/Documents.cs
@@ -12,17 +12,32 @@ namespace CMC.TS.COFF.DMS.Data.Model
         public string ContentType { get; set; } = string.Empty;
         public string Extension { get; set; } = string.Empty;
 
+        // File Storage Details
+        public long FileSize { get; set; } = 0;
+        public string StoragePath { get; set; } = string.Empty; 
+
+        // Relational Keys (FKs based on your design choice)
+        public Guid? CategoryId { get; set; }                   // Category Foreign Key
+
+        // Audit & System Metadata
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }                    // User ID who uploaded
+        public Guid? CreatedBy { get; set; }                    // User ID who uploaded
+        public bool IsDeleted { get; set; } = false;            // Soft-delete flag
         public Documents()
         {
 
         }
 
-        public Documents(string title, string? description, string contenttype, string extension)
+        public Documents(string title, string? description, string contenttype, string extension, long fileSize,string storagePath)
         {
             Title = title;
             Description = description;
             ContentType = contenttype;
             Extension = extension;
+            FileSize = fileSize;
+            StoragePath = storagePath;
         }
     }
 }
