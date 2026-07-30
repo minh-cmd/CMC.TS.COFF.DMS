1d335ce488f89ab0e60caa6439cf569f14f96f4d
bui hieu minh <thanhniende@gmail.com>
2026-07-30 14:21:04 +0700

    chore(Data/Migrations): tracking file created from add-migration operation to add unique constraint for categories code column

---
+++ CMC.TS.COFF.DMS.Data/Migrations/20260730072040_AddUniqueConstraintForCategoriesCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMC.TS.COFF.DMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintForCategoriesCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Code",
+                table: "Categories",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Code",
+                table: "Categories");
+        }
+    }
+}
