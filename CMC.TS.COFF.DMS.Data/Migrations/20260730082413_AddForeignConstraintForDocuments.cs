e47b790e138541c9a939f3268d9f4a2bd00ca809
bui hieu minh <thanhniende@gmail.com>
2026-07-30 15:27:10 +0700

    chore(Data/Migrations): adding created file from add-migration operation adding foreign key constraint for documents

---
+++ CMC.TS.COFF.DMS.Data/Migrations/20260730082413_AddForeignConstraintForDocuments.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMC.TS.COFF.DMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignConstraintForDocuments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CategoryId",
+                table: "Documents",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_CategoryId",
+                table: "Documents",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Documents_Categories_CategoryId",
+                table: "Documents",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Documents_Categories_CategoryId",
+                table: "Documents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Documents_CategoryId",
+                table: "Documents");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CategoryId",
+                table: "Documents",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
