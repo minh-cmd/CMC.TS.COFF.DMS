e6284d56011f496f834f7587e8e9aeb9a184805a
bui hieu minh <thanhniende@gmail.com>
2026-07-31 11:01:12 +0700

    feat(Biz.Model/Categories): initial and configure create-add DTO

---
+++ CMC.TS.COFF.DMS.Biz.Model/Categories/NewCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CMC.TS.COFF.DMS.Biz.Model.Categories
+{
+    public class NewCategory
+    {
+        [Required(ErrorMessage = "Categories name can't be empty")]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Categories code can't be empty")]
+        public string Code { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
